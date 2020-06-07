195be94a21acb6b8698d8b062a590a4a3a325d8d
d-georgiev-91 <d.georgiev.91@gmail.com>
2020-06-07 22:20:39 +0300

    Added MenuItems

--- samples/AcmeUIClientSide/Models/MenuItem.cs
+++ samples/AcmeUIClientSide/Models/MenuItem.cs
@@ -1,7 +1,11 @@
-﻿namespace AcmeUIClientSide.Models
+﻿using System.Collections.Generic;
+
+namespace AcmeUIClientSide.Models
 {
     public class MenuItem
     {
         public string Title { get; set; }
+
+        public IList<MenuItem> MenuItems { get; set; }
     }
 }
